e817c35ee693b8c6f304e91d02f89f265e181b65
ClonedOne <severi.giorgio@gmail.com>
2015-05-17 02:29:05 +0200

    card view implementation

---
+++ app/src/main/java/com/feedmybrain/util/NewsAdapter.java
@@ -0,0 +1,72 @@
+package com.feedmybrain.util;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.feedmybrain.R;
+import com.feedmybrain.feedlyclient.Article;
+
+import java.util.List;
+import java.util.Date;
+
+/**
+ * Created by yogaub on 17/05/15.
+ */
+public class NewsAdapter extends RecyclerView.Adapter<NewsViewHolder> {
+
+    List<Article> mDataset;
+    Context callingActivity;
+
+    public NewsAdapter(List<Article> freebleList, Context activ) {
+        callingActivity = activ;
+        mDataset = freebleList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    @Override
+    public void onBindViewHolder(NewsViewHolder fieldListViewHolder, int i) {
+        Article ci = mDataset.get(i);
+
+        //    Log.d("IMGDBG", "IMAGE NUMBER IS = " + imageIds.getResourceId(ci.getImg(), -1));
+        //    Log.d("IMGDBG", "IMAGE REFERENCE WAS = " + ci.getImg());
+
+        fieldListViewHolder.news_title.setText(ci.getTitle());
+        fieldListViewHolder.news_text.setText(ci.getContent());
+        fieldListViewHolder.news_author.setText(ci.getAuthor());
+        Date d = new Date(ci.getPublished() * 1000);
+        fieldListViewHolder.news_published.setText(d.toString());
+
+
+    }
+
+    @Override
+    public NewsViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.news_card, viewGroup, false);
+
+
+        return new NewsViewHolder(itemView);
+    }
+
+
+    public void addItem(int position, Article data) {
+        mDataset.add(position, data);
+        notifyItemInserted(position);
+    }
+
+    public void removeItem(int position) {
+        mDataset.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+}
---
+++ app/src/main/java/com/feedmybrain/util/NewsViewHolder.java
@@ -0,0 +1,40 @@
+package com.feedmybrain.util;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.feedmybrain.R;
+
+/**
+ * Created by yogaub on 17/05/15.
+ */
+public class NewsViewHolder extends RecyclerView.ViewHolder  {
+
+protected View view;
+protected TextView news_title;
+protected TextView news_text;
+protected TextView news_published;
+protected TextView news_author;
+
+
+    public NewsViewHolder (View v) {
+        super(v);
+        view = v;
+        news_title = (TextView)  v.findViewById(R.id.news_title);
+        news_text = (TextView)  v.findViewById(R.id.news_text);
+        news_published = (TextView) v.findViewById(R.id.news_published);
+        news_published = (TextView) v.findViewById(R.id.news_author);
+
+       /* view.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+            // item clicked
+            Intent intent = new Intent(view.getContext(), FieldDetailActivity.class);
+            intent.putExtra("field_id", fieldId.getText());
+            view.getContext().startActivity(intent);
+            }
+        });*/
+    }
+}
